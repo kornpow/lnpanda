0364ade11590437ed311a53893f74e9664ff34d8
Sam Korn <korn94sam@gmail.com>
2021-05-23 16:56:17 -0600

    initial upload of lnpanda

---
+++ channels.py
@@ -0,0 +1,233 @@
+from pathlib import Path
+import json
+from pprint import pprint
+import os
+import base64
+from time import sleep
+from datetime import datetime, timedelta
+import code
+
+# Pip installed Modules
+from lndgrpc import LNDClient
+import pdir
+import pandas
+from protobuf_to_dict import protobuf_to_dict
+
+pandas.set_option("display.max_colwidth", None)
+pandas.set_option("display.max_rows", None)
+pandas.options.display.float_format = "{:.8f}".format
+
+
+# from channels import lnpanda
+# a = lnpanda()
+
+class lnpanda():
+    def __init__(self):
+        self.pkdb = {}
+        self.graphnodes = {}
+        self.graphedges = {}
+
+        credential_path = Path("/home/skorn/Documents/creds/kungmeow/")
+
+        mac = str(credential_path.joinpath("admin.macaroon").absolute())
+        tls = str(credential_path.joinpath("tls.cert").absolute())
+
+        # Create the connection to the remote node
+        self.lnd = LNDClient(
+            "192.168.1.21:10009",
+            macaroon_filepath=mac,
+            cert_filepath=tls
+        )
+
+    def list_channels(self):
+        def getBalance(row):
+            return row["local_balance"] / (row["local_balance"] + row["remote_balance"])
+        def getChanSize(row):
+            return row["local_balance"] + row["remote_balance"]
+        def getToBalance(row, target=500000):
+            return target - row["local_balance"]
+
+        lnreq = protobuf_to_dict(self.lnd.list_channels())
+
+        # Check if no channels
+        if not lnreq["channels"]:
+            print("No Channels Available!")
+            return lnreq
+        # print(lnreq)
+        d = pandas.DataFrame(lnreq["channels"])
+        y = d[
+            [
+                "active",
+                "chan_id",
+                "channel_point",
+                "remote_pubkey",
+                "local_balance",
+                "remote_balance",
+                "capacity",
+            ]
+        ].fillna(0)
+        # Convert columns to integers
+        y[["local_balance", "remote_balance", "capacity"]] = y[
+            ["local_balance", "remote_balance", "capacity"]
+        ].apply(pandas.to_numeric, errors="coerce")
+        y["balanced"] = y.apply(getBalance, axis=1)
+        y["alias"] = y.apply(lambda x: self.get_alias(x.remote_pubkey), axis=1)
+        y["tobalance"] = y.apply(getToBalance, axis=1)
+        # y = y.sort_values(by=['balanced'])
+        y = y.sort_values(by=["local_balance"], ascending=False)
+        # y = y.sort_values(by=['balanced'])
+        # Get balance ratio of all channels
+        rb = y["remote_balance"].sum()
+        lb = y["local_balance"].sum()
+        print(f"Local to remote balance ratio: {lb/(lb+rb)}")
+        # y = y.set_index("channel_point")
+
+        y["remote_balance"] = y["remote_balance"].astype(int)
+        return y[[
+            "active",
+            "alias",
+            "balanced",
+            "capacity",
+            "local_balance",
+            "remote_balance",
+            "chan_id",
+            "remote_pubkey",
+        ]]
+
+    def list_payments(self):
+        t = pandas.DataFrame(protobuf_to_dict(self.lnd.list_payments(index_offset=10000))["payments"])
+        return t
+
+    def channels_with_cid(self,list_cids):
+        return self.list_channels_and_fees().query("chan_id.isin(@list_cids)")
+
+    def get_peer_pk(self, cid):
+        return self.list_channels().query("chan_id == @cid").remote_pubkey.values[0]
+
+    def get_peer_cp(self, cid):
+        return self.list_fees().query("chan_id == @cid").channel_point.values[0]
+
+    def get_peer_alias(self, cid):
+        return self.get_alias(self.get_peer_pk(cid))
+
+    def get_alias(self, pubkey):
+        try:
+            # Attempt to use index names first
+            alias = self.pkdb[pubkey]
+            return alias
+        except KeyError as e:
+            try:
+                lnreq = protobuf_to_dict(self.lnd.get_node_info(pubkey, include_channels=False))
+                alias = lnreq["node"]["alias"]
+                self.pkdb.update({pubkey: alias})
+                return lnreq["node"]["alias"]
+            except KeyError as e:
+                print(f"{pubkey} doesn't have an alias? Error: {e}")
+                return "NONE/DELETED"
+
+
+    def list_node_channels(self, pubkey):
+        lnreq = protobuf_to_dict(self.lnd.get_node_info(pubkey, include_channels=True))
+        channels = pandas.DataFrame(lnreq["channels"])
+        channels['peer_pub'] = channels.apply(lambda x: x.node1_pub if x.node1_pub != pubkey else x.node2_pub, axis=1)
+        channels = channels[['channel_id', 'chan_point', 'peer_pub',"capacity"]]
+        channels["alias"] = channels.apply(lambda x: self.get_alias(x.peer_pub), axis=1)
+        channels = channels[['alias', 'channel_id', 'chan_point', 'peer_pub',"capacity"]]
+        return channels
+
+
+    def graph_ingest(self):
+        c = self.lnd.describe_graph()
+        graph = protobuf_to_dict(c)
+
+        pubkeys = graph["nodes"]
+        pubkeys_frame = pandas.DataFrame(pubkeys)
+        pubkeys_frame.last_update = pubkeys_frame.last_update.fillna(0).astype(int)
+        # pubkeys_frame = pubkeys_frame.query("last_update is not NAN")
+        pubkeys_frame = pubkeys_frame[['pub_key', 'alias']]
+
+
+        edges = graph["edges"]
+        edges_frame = pandas.DataFrame(edges)
+        edges_frame = edges_frame[['channel_id', 'chan_point', 'node1_pub', 'node2_pub', 'capacity']]
+        edges_frame.query("node1_pub == @testpk or node2_pub == @testpk").count()
+
+
+
+    def get_block_height(self):
+        return protobuf_to_dict(self.lnd.get_info())["block_height"]
+
+
+    def get_my_pk(self):
+        return protobuf_to_dict(self.lnd.get_info())["identity_pubkey"]
+
+    def list_onchain_txns(self):
+        txns_dict = protobuf_to_dict(self.lnd.list_transactions())
+        txns = pandas.DataFrame(txns_dict["transactions"])
+        txns = txns[["tx_hash","time_stamp","label","amount","total_fees","num_confirmations","block_height"]].fillna(0)
+        txns = txns.convert_dtypes(convert_integer=True)
+        return txns[::-1]
+
+
+    def list_offchain_txns(self):
+        payments_dict = protobuf_to_dict(self.lnd.list_payments())
+        payments = pandas.DataFrame(payments_dict["payments"])
+        payments = payments[["payment_hash", "creation_date", "value", "fee_msat", "status", "payment_index"]].convert_dtypes(convert_integer=True)
+        payments["creation_date"] = payments.creation_date.apply(lambda x: datetime.fromtimestamp(x) )
+
+
+    def list_fees(self):
+        fees_dict = protobuf_to_dict(self.lnd.fee_report())
+        fees = pandas.DataFrame(fees_dict["channel_fees"])
+        return fees
+
+    def list_forwards(self):
+        forwards_dict = protobuf_to_dict(self.lnd.forwarding_history(num_max_events=20000))
+        forwards = pandas.DataFrame(forwards_dict["forwarding_events"])
+
+        forwards['datetime'] = forwards.timestamp.apply(lambda x: datetime.fromtimestamp(x))
+        forwards.drop("fee",axis=1, inplace=True)
+        return forwards
+
+    def list_channels_and_fees(self):
+        frame = a.list_channels().merge(a.list_fees(), on=["chan_id"])
+
+        frame = frame[[
+            'chan_id','active', 'alias', 'balanced',
+            'capacity', 'local_balance','remote_balance',
+            'base_fee_msat','fee_per_mil','fee_rate'
+        ]]
+        return frame
+
+
+    # TODO: NEED ONCHAIN MODULE
+    # def listCoins(self, min_confs=0, show_columns=False, add_columns=None):
+    #     a = LNDRestAPI()
+    #     url = f"/v1/utxos?min_confs={min_confs}&max_confs={getBlockHeight()}"
+    #     lnreq = a.get(url)
+
+    #     lnreq = lnd.list_coins()
+    #     print(f"Received message: {pformat(lnreq)}")
+    #     # Guard Clause
+    #     if "utxos" not in lnreq.keys():
+    #         print("No UTXOs available")
+    #         return
+    #     lnframe = pandas.DataFrame(lnreq["utxos"])
+    #     default_columns = ["address_type", "address", "amount_sat", "confirmations"]
+    #     if add_columns != None:
+    #         default_columns = default_columns + add_columns
+    #     if show_columns:
+    #         print(lnframe.columns)
+    #     return lnframe[default_columns]
+
+
+# great one-liner
+# a.list_channels().merge(a.list_fees(), on=["chan_id"])[['chan_id','active', 'alias', 'balanced', 'capacity', 'local_balance','remote_balance','base_fee_msat','fee_per_mil','fee_rate']]
+
+# update fees from a list of channel ids
+# t = list(a.list_channels().merge(a.list_fees(), on=["chan_id"])[['chan_id','active', 'alias', 'balanced', 'capacity', 'local_balance','remote_balance','base_fee_msat','fee_per_mil','fee_rate']].query("balanced > 0.6").chan_id)
+# a.list_fees().query("chan_id.isin(@t)").apply(lambda x: a.lnd.update_channel_policy(chan_point=x["channel_point"],base_fee_msat=1,fee_rate=0.000250,time_lock_delta=20) ,axis=1)
+
+if __name__ == "__main__":
+    a = lnpanda()
+    code.interact(local=locals())
